8ad829a3d07c327a1d4b1c982750665e3bf4670e
Rachel S Kim <106638262+loafcheck@users.noreply.github.com>
2024-02-22 12:30:57 -0800

    Time: 54 ms (55.63%), Space: 49.1 MB (19.67%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,46 +6,41 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    
-if (m == 0) {
-    for (let i = 0 ; i < n ; i++) {
-        nums1[i] = nums2[i];
+    if (m == 0) {
+        for (let i = 0 ; i < n ; i++) {
+            nums1[i] = nums2[i];
+        }
+        return;
     }
-    return;
-}
 
-if (n == 0) {
-    for (let i = 0 ; i < m ; i++) {
-        nums1[i] = nums1[i];
-    }
-    return;
-}
-    
-    let i = m - 1
-    let j = n - 1
+    let i = m - 1;
+    let j = n - 1;
     let k = m + n - 1;
 
-while ( i >= 0 && j >= 0) {
-    if (nums1[i] > nums2[j]) {
-    nums1[k] = nums1[i];
-        i--;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
         k--;
     }
-    else {
-        nums1[k] = nums2[j];
-        k--;
-        j--;
-    }
-}
+
     while (j >= 0) {
         nums1[k] = nums2[j];
         j--;
         k--;
     }
-    while (i >= 0) {
-        nums1[k] = nums1[i];
-        i--;
-        k--;
-    }
 
-}
+    return nums1;
+};
+
+// Example usage:
+let nums1 = [4, 0, 0, 0, 0, 0];
+let m = 1;
+let nums2 = [1, 2, 3, 5, 6];
+let n = 5;
+
+console.log(merge(nums1, m, nums2, n)); // Output: [1, 2, 3, 4, 5, 6]
