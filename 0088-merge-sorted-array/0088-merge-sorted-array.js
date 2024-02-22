7e052315f1e7ef429e80c7347d7d7cd6b83dc2e2
Rachel S Kim <106638262+loafcheck@users.noreply.github.com>
2024-02-22 12:07:55 -0800

    Time: 52 ms (66.92%), Space: 48.8 MB (44.57%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    
+if (m == 0) {
+    for (let i = 0 ; i < n ; i++) {
+        nums1[i] = nums2[i];
+    }
+    return;
+}
+
+if (n == 0) {
+    for (let i = 0 ; i < m ; i++) {
+        nums1[i] = nums1[i];
+    }
+    return;
+}
+    
+    let i = m - 1
+    let j = n - 1
+    let k = m + n - 1;
+
+while ( i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+    nums1[k] = nums1[i];
+        i--;
+        k--;
+    }
+    else {
+        nums1[k] = nums2[j];
+        k--;
+        j--;
+    }
+}
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+    while (i >= 0) {
+        nums1[k] = nums1[i];
+        i--;
+        k--;
+    }
+
+}
