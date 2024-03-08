730e9f4af15c0258ad534003976c27341dcd6b6e
Rachel S Kim <106638262+loafcheck@users.noreply.github.com>
2024-03-07 16:44:18 -0800

    Time: 122 ms (11.60%), Space: 52.8 MB (17.20%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -6,12 +6,13 @@
 
 
 var removeDuplicates = function(nums) {
-    for(let i = 0; i < nums.length;  i++){
-        var left = i; 
-        if(nums[left]== nums[i+1]) {
+    for (let i = 0 ; i < nums.length; i++) {
+        var left = i;
+        if(nums[left] == nums[i+1]) {
             nums.splice(i,1);
             i--;
+
         }
-    }  
+    }
 
 };
