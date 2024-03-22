4d2d5db69f049771481ae0f4d6b326b29fea7af2
Rachel S Kim <106638262+loafcheck@users.noreply.github.com>
2024-03-22 14:00:58 -0700

    Time: 69 ms (80.15%), Space: 58.9 MB (63.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -1,13 +1,20 @@
 var maxProfit = function(prices) {
-    let minPrice = Infinity;
+    let minPrice = prices[0];
     let maxProfit = 0;
     
-    for (let price of prices) {
-        minPrice = Math.min(minPrice, price);
-        maxProfit = Math.max(maxProfit, price - minPrice);
+    for (let i = 1; i < prices.length; i++) {
+        let currentPrice = prices[i];
+        if (currentPrice < minPrice) {
+            minPrice = currentPrice;
+        } else {
+            let potentialProfit = currentPrice - minPrice;
+            if (potentialProfit > maxProfit) {
+                maxProfit = potentialProfit;
+            }
+        }
     }
     
     return maxProfit;
 };
 
-console.log(maxProfit([7, 6, 4, 3, 1])); // Output: 0
+console.log(maxProfit([7,6,4,3,1])); // Output: 0
